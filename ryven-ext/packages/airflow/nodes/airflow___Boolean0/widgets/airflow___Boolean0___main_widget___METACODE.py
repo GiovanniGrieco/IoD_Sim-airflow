363c46b75ffcf1cacb6ebd34571d83c152322518
Giovanni Grieco <giovanni@grieco.dev>
2020-12-27 18:33:50 +0100

    Add first nodes to configure the scenario

---
+++ ryven-ext/packages/airflow/nodes/airflow___Boolean0/widgets/airflow___Boolean0___main_widget___METACODE.py
@@ -0,0 +1,45 @@
+import pathlib
+
+from NIWENV import *
+package_path = pathlib.Path(widget_pp(__file__)).as_posix()
+
+from PySide2.QtWidgets import QCheckBox
+
+class %CLASS%(QCheckBox, MWB):
+    def __init__(self, params):
+        MWB.__init__(self, params)
+        QCheckBox.__init__(self, "")
+
+        print(package_path)
+
+        self.setStyleSheet(f'''
+        QCheckBox {{
+            spacing: 90px;
+            background-color: transparent;
+        }}
+        QCheckBox::indicator {{
+            background-color: #bcbbf2;
+        }}
+        QCheckBox::indicator:checked {{
+            image: url("{package_path}/resources/checkbox_checked.png");
+        }}
+        QCheckBox::indicator:unchecked {{
+            image: url("{package_path}/resources/checkbox_unchecked.png");
+        }}
+        ''')
+
+        self.clicked.connect(M(self.on_click))
+
+    def on_click(self):
+        state = self.isChecked()
+        self.parent_node_instance.outputs[0].set_val(state)
+
+    def get_data(self):
+        data = {}
+        return data
+
+    def set_data(self, data):
+        pass
+
+    def remove_event(self):
+        pass
---
+++ ryven-ext/packages/airflow/nodes/airflow___SaveToDirectory0/widgets/airflow___SaveToDirectory0___DirectoryChooser___METACODE.py
@@ -0,0 +1,65 @@
+from NIWENV import *
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QPushButton, QFileDialog
+
+
+class %CLASS%(QWidget, IWB):
+    def __init__(self, params):
+        super(%CLASS%, self).__init__()
+        #IWB.__init__(self, params)
+
+        self.directory = ''
+
+        # UI
+        self.setLayout(QVBoxLayout())
+        self.path_line_edit = QLineEdit()
+        self.path_line_edit.setPlaceholderText('directory...')
+        self.path_line_edit.textChanged.connect(M(self.set_directory_path))
+        self.select_directory_button = QPushButton('select')
+        self.select_directory_button.clicked.connect(M(self.select_directory_button_clicked))
+        self.layout().addWidget(self.path_line_edit)
+        self.layout().addWidget(self.select_directory_button)
+
+        self.setStyleSheet('''
+            QWidget {
+                background: transparent;
+            }
+            QLineEdit {
+                border-radius: 10px;
+                background-color: transparent;
+                border: 1px solid #202020;
+                color: #aaaaaa;
+                padding: 3px;
+            }
+            QPushButton {
+                background-color: #36383B;
+                padding-top: 5px;
+                padding-bottom: 5px;
+                padding-left: 22px;
+                padding-right: 22px;
+                border: 1px solid #666666;
+                border-radius: 5px;
+            }
+        ''')
+        self.setFixedWidth(150)
+
+    def select_directory_button_clicked(self):
+        directory_path = QFileDialog.getExistingDirectory(self, 'Select Directory')
+        if len(directory_path) > 0:
+            self.set_directory_path(directory_path)
+
+    def set_directory_path(self, new_directory_path):
+        self.directory = new_directory_path
+        self.path_line_edit.setText(self.directory)
+
+    def get_val(self):
+        return self.directory
+
+    def get_data(self):
+        return self.get_val()
+
+    def set_data(self, data):
+        self.set_directory_path(data)
+
+    # remove logs and stop threads and timers here
+    def remove_event(self):
+        pass
